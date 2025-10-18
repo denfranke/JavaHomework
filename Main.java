aea601492ec5d6ba4fffbfb7b1c7c28428527f89
Denis <33222627+denfranke@users.noreply.github.com>
2025-10-18 17:50:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
-package Homework_1.Task1;
+package Homework_1;
 
 public class Main {
     public static void main(String[] args) {
         String s = "Kulichkov Denis";
         System.out.println(s);
+
+        int income = 10000;
+        int spending = 7500;
+
+        System.out.println("Итого (руб):\n" + (income - spending));
     }
 }
