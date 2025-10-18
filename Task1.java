f8aaa8b279a41bcb420e03ed09f1ec86bef36258
Denis <33222627+denfranke@users.noreply.github.com>
2025-10-18 11:44:24 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,7 @@
+package Homework_1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Kulichkov Denis");
+    }
+}
---
+++ Task2.java
@@ -0,0 +1,9 @@
+package Homework_1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int income = 44000;
+        int spending= 43000;
+        System.out.println("Итого (руб):\n"+ (income - spending));
+    }
+}
