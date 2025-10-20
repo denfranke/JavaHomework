53e648eac964335551c04b8028c643c98084429a
Denis <33222627+denfranke@users.noreply.github.com>
2025-10-20 10:49:34 +0300

    Update CustomsService.java

--- CustomsService.java
+++ CustomsService.java
@@ -3,8 +3,8 @@ package ru.netology.service;
 public class CustomsService {
     public static final int DUTY_RATE = 100;
 
-    public static int calculateCustoms(int price, int height) {
+    public static int calculateCustoms(int price, int weight) {
 
-        return price / DUTY_RATE + height * DUTY_RATE;
+        return price / DUTY_RATE + weight * DUTY_RATE;
     }
 }
