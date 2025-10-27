48c11653dae77859c132ec13e3e0321ae48ac6ce
Denis <33222627+denfranke@users.noreply.github.com>
2025-10-27 09:36:34 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,27 +3,66 @@ package Homework_3;
 import java.util.Scanner;
 
 public class Main {
+
+    public static void taxEarningsMinusSpendings(int earnings, int spendings) {
+        int tax1 = Math.max((earnings) * 6 / 100, 0);
+        int tax2 = Math.max((earnings - spendings) * 15 / 100, 0);
+
+        if (tax1 < tax2) {
+            System.out.println("\nМы советуем вам УСН доходы\n" +
+                    "Ваш налог составит: " + tax1 +
+                    "\nНалог на другой системе: " + tax2 +
+                    "\nЭкономия: " + (tax2 - tax1));
+        } else {
+            System.out.println("\nМы советуем вам УСН доходы минус расходы\n" +
+                    "Ваш налог составит: " + tax2 +
+                    "\nНалог на другой системе: " + tax1 +
+                    "\nЭкономия: " + (tax1 - tax2));
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int k = 0, year, days;
-        while (true) {
-            System.out.println("Введите год:");
-            year = scanner.nextInt();
-            System.out.println("Введите количество дней:");
-            days = scanner.nextInt();
 
-            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-                if (days != 366)
-                    System.out.println("Неправильно! В этом году 366 дней!\n" +
-                            "Набрано очков: " + k);
-                else
-                    k++;
-            else if (days != 365) {
-                System.out.println("Неправильно! В этом году 365 дней!\n" +
-                        "Набрано очков: " + k);
+        int earnings = 0, spendings = 0;
+        String input;
+
+        while (true) {
+            System.out.println("\nВыберите операцию и введите её номер:\n" +
+                    "1. Добавить новый доход\n" +
+                    "2. Добавить новый расход\n" +
+                    "3. Выбрать систему налогообложения");
+
+            input = scanner.nextLine();
+
+            if ("end".equals(input)) {
                 break;
-            } else
-                k++;
+            }
+
+            int operation = Integer.parseInt(input);
+            int money = 0;
+            String moneyStr;
+
+            switch (operation) {
+                case 1:
+                    System.out.println("Введите сумму дохода:");
+                    moneyStr = scanner.nextLine();
+                    money = Integer.parseInt(moneyStr);
+                    earnings += money;
+                    break;
+                case 2:
+                    System.out.println("Введите сумму расходов:");
+                    moneyStr = scanner.nextLine();
+                    money = Integer.parseInt(moneyStr);
+                    spendings += money;
+                    break;
+                case 3:
+                    taxEarningsMinusSpendings(earnings, spendings);
+                    break;
+                default:
+                    System.out.println("Такой операции нет");
+            }
         }
+        System.out.println("Программа завершена!");
     }
 }
