96ee64c9ccad7780310a1b5a7e16b4df36f0efcf
Denis <33222627+denfranke@users.noreply.github.com>
2025-11-06 09:54:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,44 @@
-package Homework_6;
+package Homework_7;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Book book = new Book("Denis sam sdelal etot kod", 12, new Author("Max","Ming", 5), 50);
-        System.out.println(book.isBig() ? ">=500" : "<500");
-        System.out.println(book.matches("sam") ? "Слово найдено" : "Словов не найдено");
-        System.out.println("Цена: " + book.estimatePrice());
+        String[] products = {"Хлеб", "Яблоки", "Молоко"};
+        int[] prices = {100, 200, 300};
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nСписок товаров для покупки:");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println(i + 1 + ". " + products[i] + " " + prices[i] + " руб/шт");
+        }
+
+        int sumProducts = 0, j = 0;
+        int[] prdctNum = new int[products.length];
+        int[] cnt = new int[products.length];
+
+        while (true) {
+            System.out.println("Выберите товар и количество или введите end: ");
+
+            String input = scanner.next();
+
+            if (input.equals("end")) {
+                break;
+            }
+            int productNumber = Integer.parseInt(input);
+            prdctNum[j] = productNumber - 1;
+            int productCount = Integer.parseInt(scanner.next());
+            cnt[j] = productCount;
+            j++;
+
+            sumProducts += prices[productNumber - 1] * productCount;
+        }
+        System.out.println("Ваша корзина:");
+        for (int i = 0; i < j; i++) {
+            System.out.println(products[prdctNum[i]] + " " + cnt[i] + " шт " + prices[prdctNum[i]] + " руб/шт " + (prices[prdctNum[i]] * cnt[i]) + "руб в сумме");
+        }
+        System.out.println("Итого " + sumProducts);
+        scanner.close();
     }
 }
