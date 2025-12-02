0775f150d5737684e85fbcb16310b33916a27802
Denis <33222627+denfranke@users.noreply.github.com>
2025-12-02 09:10:25 +0300

    Delete taxes/Sale.java

--- taxes/Sale.java
+++
@@ -1,7 +0,0 @@
-package taxes;
-
-public class Sale extends Deal {
-    public Sale(String productName, int price) {
-        super("Продажа «" + productName + "» на " + price + " руб.", price, 0);
-    }
-}
