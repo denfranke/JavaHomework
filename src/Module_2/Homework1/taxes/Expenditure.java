a4a708b7a3807ea8523d1432ece94a65694fb6f0
Denis <33222627+denfranke@users.noreply.github.com>
2025-12-11 11:43:06 +0300

    Delete src/Module_2/Homework1/taxes/Expenditure.java

--- src/Module_2/Homework1/taxes/Expenditure.java
+++
@@ -1,8 +0,0 @@
-package Module_2.Homework1.taxes;
-
-public class Expenditure extends Deal {
-    public Expenditure(String productName, int price) {
-        super("Покупка «" + productName + "» на " + price + " руб.", 0, price);
-    }
-}
-
